45304dc5a7ae204606b58a8a3d71f651702d9872
EDUARDO BARTELLT CASANOVA <eduardo_casanova@estudante.sesisenai.org.br>
2026-05-27 15:39:44 -0300

    feat início do projeto, criação do Next e modelo genérico para testes com Supabase (sucesso) 27/05

---
+++ src/app/api/loja/[id]/route.ts
@@ -0,0 +1,15 @@
+import { supabase } from '@/lib/supabase'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+  const body = await req.json()
+  const { data, error } = await supabase.from('loja').update(body).eq('id', params.id).select()
+  if (error) return NextResponse.json({ error }, { status: 500 })
+  return NextResponse.json(data[0])
+}
+
+export async function DELETE(_: NextRequest, { params }: { params: { id: string } }) {
+  const { error } = await supabase.from('loja').delete().eq('id', params.id)
+  if (error) return NextResponse.json({ error }, { status: 500 })
+  return NextResponse.json({ message: 'Deletado com sucesso' })
+}
---
+++ src/app/api/loja/route.ts
@@ -0,0 +1,15 @@
+import { supabase } from '@/lib/supabase'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET() {
+  const { data, error } = await supabase.from('loja').select('*')
+  if (error) return NextResponse.json({ error }, { status: 500 })
+  return NextResponse.json(data)
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json()
+  const { data, error } = await supabase.from('loja').insert(body).select()
+  if (error) return NextResponse.json({ error }, { status: 500 })
+  return NextResponse.json(data[0], { status: 201 })
+}
